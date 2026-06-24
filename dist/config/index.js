cb9d15c0cbd70374dac5901feee733894e6f6a85
kevianto <kevinshahale746@gmail.com>
2026-06-24 12:45:06 +0300

    added

--- dist/config/index.js
+++ dist/config/index.js
@@ -8,9 +8,9 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.config = {
     port: parseInt(process.env.PORT || '3000', 10),
-    mqttBrokerUrl: process.env.MQTT_BROKER_URL || 'mqtt://localhost:1883',
+    mqttBrokerUrl: process.env.MQTT_BROKER_URL || 'mqtt://broker.hivemq.com:1883',
     mqttClientId: process.env.MQTT_CLIENT_ID || 'smart-home-backend',
-    mqttLedTopic: 'home/led',
-    mqttFanTopic: 'home/fan',
-    mqttStatusTopic: 'home/status',
+    mqttLedTopic: process.env.MQTT_LED_TOPIC || 'champions_e4t2/home/led',
+    mqttFanTopic: process.env.MQTT_FAN_TOPIC || 'champions_e4t2/home/fan',
+    mqttStatusTopic: process.env.MQTT_STATUS_TOPIC || 'champions_e4t2/home/status',
 };
